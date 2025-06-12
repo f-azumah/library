b504b235da373e5870078bc5f8dbf393cd7cde09
f-azumah <fiona.azumah@gmail.com>
2025-06-12 18:38:27 -0400

    included new functionality for deleting books and marking books as finished

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const libDisplay = document.querySelector(".display");
+let finished = false;
 
 function Book(title, author, pages){
     if (!new.target) throw Error("You must use the 'new' operator to call this constructor");
@@ -7,18 +8,27 @@ function Book(title, author, pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = false;
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages`;
     };
 }
 
-function addbookToLibrary(title, author, pages){
+function addBookToLibrary(title, author, pages){
     const book = new Book(title, author, pages);
     myLibrary.push(book);
 }
 
-addbookToLibrary("Wahala", "Nikki May", 384);
+addBookToLibrary("Wahala", "Nikki May", 384);
+addBookToLibrary("The Three of Us", "Ore Agbaje-Williams", 150);
+addBookToLibrary("No Ordinary Love", "Myah Ariel" , 289);
+addBookToLibrary("Things Fall Apart", "Chinua Achebe", 300);
 console.log(myLibrary)
+
+Book.prototype.markFinished = function() {
+    this.isRead = !this.isRead;
+    finished = !finished
+}
 
 function displayBook(){
     libDisplay.textContent = "";
@@ -26,9 +36,46 @@ function displayBook(){
         let bookCard = document.createElement('div');
         bookCard.setAttribute(
             "style", "background-color: LightBlue; box-sizing: border-box; border-radius: 20px; width: 400px; height: 100px; box-shadow: 5px 5px 10px grey"
-        )
-        bookCard.classList.add("bookCard")
-        bookCard.textContent = myLibrary[i].info();;
+        );
+        bookCard.classList.add("bookCard");
+        bookCard.dataset.id = myLibrary[i].id;
+        bookCard.textContent = myLibrary[i].info();
+
+        let Btns = document.createElement('div');
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add("delete");
+        deleteBtn.textContent = "Remove Book";
+        Btns.appendChild(deleteBtn);
+        Btns.classList.add("cardBtns");
+        bookCard.appendChild(Btns);
+
+        deleteBtn.addEventListener("click", () => {
+            const bookId = bookCard.dataset.id;
+            const index = myLibrary.findIndex(book => book.id === bookId);
+            if (index != -1){
+                myLibrary.splice(index, 1);
+                bookCard.remove();
+            }
+        })
+
+        let toggleRead = document.createElement("button");
+        toggleRead.textContent = "Mark as finished";
+        toggleRead.classList.add("toggle");
+        Btns.appendChild(toggleRead);
+
+        toggleRead.addEventListener("click", () => {
+            myLibrary[i].markFinished();
+            if (finished === true){
+                bookCard.style.color = "white";
+                bookCard.style.backgroundColor = "#005A9C";
+                toggleRead.textContent = "Mark as unread";
+            } else {
+                bookCard.style.color = "black";
+                bookCard.style.backgroundColor = "LightBlue";
+                toggleRead.textContent = "Mark as finished";
+            }
+        })
+        
         libDisplay.appendChild(bookCard);
     }
 }
@@ -57,7 +104,7 @@ submitBtn.addEventListener("click", (e) => {
     const pages = document.querySelector("#pages");
 
     if (title && author && pages){
-        addbookToLibrary(title.value, author.value, pages.value);
+        addBookToLibrary(title.value, author.value, pages.value);
         displayBook();
     }
     title.value = "";
